136d88884e19d399fd073950cd0ffa9fd22aca61
Bartek U <2597165+jazzman55@users.noreply.github.com>
2023-06-02 12:46:02 +0200

    rename and clean up

---
+++ src/MakoIoT.Core.Configuration.Model/ConfigParamMetadata.cs
@@ -0,0 +1,33 @@
+﻿namespace MakoIoT.ConfigurationApi.Model
+{
+    public class ConfigParamMetadata
+    {
+        /// <summary>
+        /// Name of the field. Must match property name in config class.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Type of the value. Currently supported in configuration app are:
+        /// string, int, bool, float, double, datetime, timezone
+        /// </summary>
+        public string Type { get; set; }
+
+        /// <summary>
+        /// Label of the field
+        /// </summary>
+        public string Label { get; set; }
+
+        /// <summary>
+        /// True, if the field should not show on form
+        /// </summary>
+        public bool IsHidden { get; set; }
+
+        /// <summary>
+        /// True, if form field should be masked (like password)
+        /// </summary>
+        public bool IsSecret { get; set; }
+
+        public object DefaultValue { get; set; }
+    }
+}
---
+++ src/MakoIoT.Core.Configuration.Model/ConfigSectionMetadata.cs
@@ -0,0 +1,25 @@
+﻿namespace MakoIoT.ConfigurationApi.Model
+{
+    public class ConfigSectionMetadata
+    {
+        /// <summary>
+        /// Section name. Must match SectionName in the config class.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Section label.
+        /// </summary>
+        public string Label { get; set; }
+
+        /// <summary>
+        /// True, if section should be hidden on the form.
+        /// </summary>
+        public bool IsHidden { get; set; }
+
+        /// <summary>
+        /// List of section's parameters.
+        /// </summary>
+        public ConfigParamMetadata[] Parameters { get; set; }
+    }
+}
